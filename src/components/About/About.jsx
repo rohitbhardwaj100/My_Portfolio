936881bdab5a64241aabcc33e368cfd1c400d99a
rohitbhardwaj100 <rohitbhardwaj1179@gmail.com>
2025-06-15 00:49:11 +0530

    Updated About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Kt7gi2zokZl_E1Ro97CmkYZFbNdZ7fCE/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
